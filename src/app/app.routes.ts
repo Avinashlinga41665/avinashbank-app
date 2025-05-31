dbba38be47245b1a2f819b6e0d4e944e89e78239
AvinashLinga <avinashlinga41665@gmail.com>
2025-06-01 00:55:48 +0530

    secondchange

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AccountSummaryComponent  } from './pages/account-summary/account-summary.component';
+import { ForgotPasswordComponent  } from './pages/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'Register', component: RegisterComponent},
-  { path: 'AccountSummary', component: AccountSummaryComponent}
+  { path: 'AccountSummary', component: AccountSummaryComponent},
+  { path: 'ForgotPassword', component: ForgotPasswordComponent}
 
 ];
