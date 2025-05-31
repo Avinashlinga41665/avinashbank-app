ecf44ee2acc889cd95219a1343e2d2049306fa42
AvinashLinga <avinashlinga41665@gmail.com>
2025-05-31 19:13:26 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -26,10 +26,10 @@ export default {
   assets: {
     'index.csr.html': {size: 23592, hash: '9e33f9c7598887434fe2043d6b08c52296b1292c181dc15151e11be339ba0fe1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 17166, hash: '2aad325bd1356062357d7076ecf391b6eb8e758d90b44116939e4d91b70524ba', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 25194, hash: 'ca9902747145b0e22d9eb34c0fa261e996f8249d1cf483512081185dc8c3759c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'dashboard/index.html': {size: 24036, hash: '1a3890e9fa7a97536eef955f8e5eaff30579bd128441c959d2a46583de54f846', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
-    'Register/index.html': {size: 24493, hash: 'da9c2957bc704261fc47d2270f08b2cb597bc94c6baef783167ec58d4b0eee44', text: () => import('./assets-chunks/Register_index_html.mjs').then(m => m.default)},
     'AccountSummary/index.html': {size: 53748, hash: '940de3eb11a815351dc5319a297073963c905dca474974b3cb850ba10107cf7d', text: () => import('./assets-chunks/AccountSummary_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 25194, hash: 'ca9902747145b0e22d9eb34c0fa261e996f8249d1cf483512081185dc8c3759c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'Register/index.html': {size: 24493, hash: 'da9c2957bc704261fc47d2270f08b2cb597bc94c6baef783167ec58d4b0eee44', text: () => import('./assets-chunks/Register_index_html.mjs').then(m => m.default)},
     'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
   },
 };
